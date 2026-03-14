e9a8d68120631ab1cb24cb9034fcd77f587cdafa
Kirito1610 <126069337+Kirito1610@users.noreply.github.com>
2026-03-14 22:05:32 +0530

    Add video streaming functionality with FFmpeg

--- server.js
+++ server.js
@@ -2,29 +2,61 @@ const express = require("express");
 const cors = require("cors");
 
 const { getVideoInfo } = require("./services/youtube");
-
 const app = express();
 app.use(cors());
 app.use(express.static("public"));
+const ffmpeg = require("fluent-ffmpeg");
+const ffmpegStatic = require("ffmpeg-static");
+
+// Tell fluent-ffmpeg where the binary is
+ffmpeg.setFfmpegPath(ffmpegStatic);
 
 const PORT = process.env.PORT || 3000;
 
 app.get("/api/video/:id", async (req, res) => {
-
   try {
-
     const data = await getVideoInfo(req.params.id);
 
     res.json(data);
-
   } catch (err) {
-
     res.status(500).json({
-      error: err.message
+      error: err.message,
     });
+  }
+});
 
+app.get("/stream", async (req, res) => {
+  try{
+    const {id}=req.query;
+   const data = await getVideoInfo(id);
+
+  if (!data.audio) {
+    return res.status(400).send("videoUrl and audioUrl required");
   }
 
+  res.setHeader("Content-Type", "video/mp4");
+   ffmpeg()
+      .input(data.videos[3].videoUrl)
+      .input(data.audio)
+      .outputOptions([
+        "-c:v copy",
+        "-c:a aac",
+        "-movflags frag_keyframe+empty_moov+faststart",
+      ])
+      .format("mp4")
+      .on("error", (err) => {
+        console.error("FFmpeg error:", err.message);
+        if (!res.headersSent) {
+          res.status(500).json({ error: "Streaming failed" });
+        }
+      })
+      .pipe(res, { end: true });
+    } catch (error) {
+    console.error("Stream error:", error.message);
+    if (!res.headersSent) {
+      res.status(500).json({ error: error.message });
+    }
+  }
 });
 
 app.listen(PORT, () => {
