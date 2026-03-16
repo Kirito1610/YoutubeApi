9a8f6a34cd94585e899e42069fcdf873abac3462
Kirito1610 <126069337+Kirito1610@users.noreply.github.com>
2026-03-16 11:34:07 +0530

    Remove video streaming and FFmpeg processing
    Removed video streaming functionality and FFmpeg integration.

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-
 const { getVideoInfo } = require("./services/youtube");
 const app = express();
 app.use(cors());
-app.use(express.static("public"));
-const { spawn } = require("child_process");
-const ffmpegPath = require("ffmpeg-static");
+
 
 
 const PORT = process.env.PORT || 3000;
@@ -22,46 +19,6 @@ app.get("/api/video/:id", async (req, res) => {
     });
   }
 });
-
-app.get("/stream/:id", async(req, res) => {
-  const data = await getVideoInfo(req.params.id);
-
-  // Important for streaming
-  res.setHeader("Content-Type", "video/mp4");
-  res.setHeader("Transfer-Encoding", "chunked");
-  res.setHeader("Cache-Control", "no-cache");
-  res.flushHeaders();
-  const ffmpeg = spawn(ffmpegPath, [
-      "-i", data.videos[4].videoUrl,
-      "-i", data.audio,,
-      "-c:v", "copy",
-      "-c:a", "aac",
-      "-f", "hls",
-      "-hls_time", "4",
-      "-hls_list_size", "0",
-      path.join(outputDir, "stream.m3u8")
-    ]);
-
-  ffmpeg.stdout.pipe(res);
-
-ffmpeg.stdout.on("error", (err) => {
-  console.error("FFmpeg stdout error:", err);
-});
-
-ffmpeg.stderr.on("data", (data) => {
-  console.error("FFmpeg stderr:", data.toString());
-});
-
-ffmpeg.on("error", (err) => {
-  console.error("FFmpeg process error:", err);
-});
-
-ffmpeg.on("close", (code, signal) => {
-  console.log("FFmpeg closed with code:", code, "signal:", signal);
-  res.end();
-});
-});
-
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
